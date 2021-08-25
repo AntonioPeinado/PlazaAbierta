5bc4d9ddd1d36a83cc491e3a1b2e028baeeb9ae9
elcaco <a.peinado.sevilla@gmail.com>
2021-08-25 17:59:10 +0200

    tarjetas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,92 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import code from '../img/code.jpg';
 
 const Home = () =>{
   return (
     <>
       <div className="home">
-        
-      
-          <h1>Home</h1>
-      
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={code}
+            style={{ width: "17rem", height: "7rem" }}
+          />
+          <Card.Body>
+            <Card.Title>Tiendas Ropa</Card.Title>
+            <Card.Text>
+             Desde aquí puede pasear por todas las tiendas de ropa de Pinto,
+             y decidir cual quiere ver 
+            </Card.Text>
+            <Button variant="primary">Ir a Ropa</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={code}
+            style={{ width: "17rem", height: "7rem" }}
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={code}
+            style={{ width: "17rem", height: "7rem" }}
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={code}
+            style={{ width: "17rem", height: "7rem" }}
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={code}
+            style={{ width: "17rem", height: "7rem" }}
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
       <div className="home-footer">
         <Footer />
